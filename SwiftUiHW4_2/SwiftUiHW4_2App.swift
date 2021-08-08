62123cf9728fdb06075c6e24b5d73e9eeea8204d
Demiar <alexey174@gmail.com>
2021-08-08 11:03:47 +0300

    Initial Commit

---
+++ SwiftUiHW4_2/SwiftUiHW4_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiHW4_2App.swift
+//  SwiftUiHW4_2
+//
+//  Created by Алексей on 08.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiHW4_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
